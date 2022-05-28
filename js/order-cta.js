c08bdf20260a5860408fa48475c7efa684e08724
goumi1009 <goumi1009@gmail.com>
2022-05-29 01:08:54 +0900

    🚧 Implement toggling bookmark button

--- js/order-cta.js
+++ js/order-cta.js
@@ -14,6 +14,28 @@ const closeOrderModal = () => {
   orderModalOverlay.classList.remove('is-active');
 };
 
-orderCtaBuyButton.addEventListener('click', openOrderModal);
+const toggleOrderCtaBookmark = (e) => {
+  const target = e.currentTarget;
+  const [icon, countSpan] = target.children;
+  const count = Number(countSpan.innerHTML.replaceAll(',', ''));
+  const isActive = target.classList.contains('is-active');
 
+  let newCount = count;
+
+  if (isActive) {
+    icon.setAttribute('class', 'ic-bookmark');
+    newCount -= 1;
+  } else {
+    icon.setAttribute('class', 'ic-bookmark-filled');
+    newCount += 1;
+  }
+
+  countSpan.innerHTML = newCount.toLocaleString();
+  countSpan.setAttribute('aria-label', `북마크 ${newCount.toLocaleString()}회`);
+  target.classList.toggle('is-active');
+};
+
+orderCtaBuyButton.addEventListener('click', openOrderModal);
 orderModalOverlay.addEventListener('click', closeOrderModal);
+
+orderCtaBookmarkButton.addEventListener('click', toggleOrderCtaBookmark);
